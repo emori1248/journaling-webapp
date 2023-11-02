359812db2bafbe504f76bfca28b0fe43a72a043c
Ethan Mori <egmori2@gmail.com>
2023-11-02 16:43:33 -0400

    Fixed pg in package manager and corresponding imports

--- src/pages/api/getTodos.ts
+++ src/pages/api/getTodos.ts
@@ -1,8 +1,9 @@
 import { TodosResponseData } from "@/api/todos";
 import { getAuth } from "@clerk/nextjs/server";
 import type { NextApiRequest, NextApiResponse } from "next";
+import {Client} from "pg"
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   // The type for the response is defined in src/api/todos.ts with the query functions
   res: NextApiResponse<TodosResponseData>
@@ -15,57 +16,22 @@ export default function handler(
     return res.status(401).json({ error: "Unauthorized" });
   }
 
-  // This is an example for making use of their id in the server, you would probably want to use the SQL adapter here
-  // with some sort of query like "selectimport { TodosResponseData } from "@/api/todos";
-import { getAuth } from "@clerk/nextjs/server";
-import type { NextApiRequest, NextApiResponse } from "next";
-import * as pg from 'pg'
-const { Pool } = pg
-require('dotenv').config()
+  const conString = process.env.DB_CONNECTION_STRING
+  const client = new Client({connectionString:conString});
+  await client.connect()
 
-export default function handler(
-  req: NextApiRequest,
-  // The type for the response is defined in src/api/todos.ts with the query functions
-  res: NextApiResponse<TodosResponseData>
-) {
-  // this gets the clerk userid from the user's request headers
-  const { userId } = getAuth(req);
-
-  // If they have no id (not logged in) send a 401
-  if (!userId) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-
-  // This is an example for making use of their id in the server, you would probably want to use the SQL adapter here
-  // with some sort of query like "select * from posts where authorid is userid" or whatever the syntax might be exactly
-  // and then return it as a big json object (which would need to be updated in the datatype accordingly)
-
-  var conString = process.env.DB_CONNECTION_STRING
-  var client = new pg.Client(conString);
-
+  // TODO https://node-postgres.com/features/queries Check out this documentation page for using their query builder to avoid injection avenues.
+  
   // This is a big block of test data in place of a db query result
-  const notes = [
-    client.connect(function(err) {
-      if(err) {
-        return console.error('could not connect to postgres', err);
-      }
-      client.query('SELECT * FROM public.posts', function(err, result) {
-        if(err) {
-          return console.error('error running query', err);
-        }
-        console.log(result.rows);
-        // >> output: 2018-08-23T14:02:57.117Z
-      });
-    }),
-  ] as TodosResponseData;
+  /* 
+  TODO I just added this back here so that you can console log the query data you're getting and so that the webapp is getting data
+  TODO in the meantime that you can replace.
 
-  // And we return the data with a 200 status
-  res.status(200).json({ todos: notes });
-}
- * from posts where authorid is userid" or whatever the syntax might be exactly
-  // and then return it as a big json object (which would need to be updated in the datatype accordingly)
-
-  // This is a big block of test data in place of a db query result
+  TODO also make sure to handle errors and return a 500 internal server error as specified here https://node-postgres.com/#error-handling
+  TODO if the query runs into any issues. 
+  
+  TODO Lastly, I changed this function to be async up at the top since it'll want to be using async/await to block intelligently on the db calls
+  */
   const notes = [
     {
       id: "1234abcd",
