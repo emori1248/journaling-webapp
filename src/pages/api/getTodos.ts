0608d671c389110f763cf3cf9a290990b25e6911
Dean0lew <dean0lew@gmail.com>
2023-11-01 22:23:50 -0400

    Update getTodos.ts with error on line 45, todos type not matching

--- src/pages/api/getTodos.ts
+++ src/pages/api/getTodos.ts
@@ -16,7 +16,53 @@ export default function handler(
   }
 
   // This is an example for making use of their id in the server, you would probably want to use the SQL adapter here
+  // with some sort of query like "selectimport { TodosResponseData } from "@/api/todos";
+import { getAuth } from "@clerk/nextjs/server";
+import type { NextApiRequest, NextApiResponse } from "next";
+import * as pg from 'pg'
+const { Pool } = pg
+require('dotenv').config()
+
+export default function handler(
+  req: NextApiRequest,
+  // The type for the response is defined in src/api/todos.ts with the query functions
+  res: NextApiResponse<TodosResponseData>
+) {
+  // this gets the clerk userid from the user's request headers
+  const { userId } = getAuth(req);
+
+  // If they have no id (not logged in) send a 401
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  // This is an example for making use of their id in the server, you would probably want to use the SQL adapter here
   // with some sort of query like "select * from posts where authorid is userid" or whatever the syntax might be exactly
+  // and then return it as a big json object (which would need to be updated in the datatype accordingly)
+
+  var conString = process.env.DB_CONNECTION_STRING
+  var client = new pg.Client(conString);
+
+  // This is a big block of test data in place of a db query result
+  const notes = [
+    client.connect(function(err) {
+      if(err) {
+        return console.error('could not connect to postgres', err);
+      }
+      client.query('SELECT * FROM public.posts', function(err, result) {
+        if(err) {
+          return console.error('error running query', err);
+        }
+        console.log(result.rows);
+        // >> output: 2018-08-23T14:02:57.117Z
+      });
+    }),
+  ] as TodosResponseData;
+
+  // And we return the data with a 200 status
+  res.status(200).json({ todos: notes });
+}
+ * from posts where authorid is userid" or whatever the syntax might be exactly
   // and then return it as a big json object (which would need to be updated in the datatype accordingly)
 
   // This is a big block of test data in place of a db query result
