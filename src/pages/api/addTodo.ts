8ad4c89ce6009794235ae319810a1e17eaaa85c5
Ethan Mori <egmori2@gmail.com>
2023-11-06 19:06:06 -0500

    change insert query to return result

--- src/pages/api/addTodo.ts
+++ src/pages/api/addTodo.ts
@@ -1,4 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { getAuth } from "@clerk/nextjs/server";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Client } from "pg";
@@ -16,11 +15,9 @@ export type TodoResponseData = {
 
 // TODO not implemented
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<TodoResponseData>
+  req: NextApiRequest,
+  res: NextApiResponse<TodoResponseData>
 ) {
-
-
   const { userId } = getAuth(req);
 
   // If they have no id (not logged in) send a 401
@@ -29,34 +26,34 @@ export default async function handler(
   }
 
   const conString = process.env.DB_CONNECTION_STRING;
-  const client = new Client({connectionString:conString});
+  const client = new Client({ connectionString: conString });
   await client.connect();
 
   try {
     const query = {
       // give the query a unique name
-      name: 'create-empty-todo',
-      text: 'INSERT INTO public.posts(post_title, user_id, post, post_id) VALUES($1, $2, $3, DEFAULT)',
+      name: "create-empty-todo",
+      text: "INSERT INTO public.posts(post_title, user_id, post, post_id) VALUES($1, $2, $3, DEFAULT) RETURNING *",
       values: ["New Note", userId, ""],
-    }
+    };
 
     // Should only ever be one row created by this query
     const row = (await client.query(query)).rows[0];
-    console.log(row);
 
     const updatedRow = {
       id: row.post_id,
       name: row.post_title,
       updated_at: row.post_date,
       author_id: row.user_id,
-      content: row.post
-    }
-    console.log(updatedRow) 
+      content: row.post,
+    };
+    console.log("updated row");
+    console.log(updatedRow);
     return res.status(200).json({ todo: updatedRow });
- } catch (err) {
+  } catch (err) {
     console.error(err);
-    return res.status(500).json({error : "Error connecting to database"})
- } finally {
-    await client.end()
- }
+    return res.status(500).json({ error: "Error connecting to database" });
+  } finally {
+    await client.end();
+  }
 }
