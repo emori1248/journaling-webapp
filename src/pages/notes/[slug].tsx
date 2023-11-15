1a7e09ec47a99f1b1c6aef3e5ce7dcbe135fb14c
Ethan Mori <egmori2@gmail.com>
2023-11-14 22:20:28 -0500

    Removed fetch from prerender

--- src/pages/notes/[slug].tsx
+++ src/pages/notes/[slug].tsx
@@ -6,36 +6,70 @@ import { SubmitHandler, useForm } from "react-hook-form";
 
 export default function NotePage() {
   const router = useRouter();
-  const slug = router.query.slug
-  const query = useQuery(["todoById", slug], () => getTodoById(slug as string)); // TODO change this force cast later
 
-  type PostInputs = {
-    name: string;
-    content: string;
-  };
+  if (!router.isReady) return;
 
-  const onSubmit: SubmitHandler<PostInputs> = (data) => {
-    console.log({...data, id: router.query.slug});
-    // mutation.mutate({ ...data });
-  };
+  const slug = router.query.slug;
+  console.log(slug);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<PostInputs>();
+  function NoteForm() {
+    const query = useQuery(["todoById", slug], () =>
+      getTodoById(slug as string)
+    ); // TODO change this force cast later
 
+    type PostInputs = {
+      name: string;
+      content: string;
+    };
 
-  if (!query.data) {
-    return;
+    const onSubmit: SubmitHandler<PostInputs> = (data) => {
+      console.log({ ...data, id: router.query.slug });
+      // mutation.mutate({ ...data });
+    };
+
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<PostInputs>();
+
+    if (!query.data) {
+      return;
+    }
+
+    const { todo } = query.data;
+
+    // TODO
+    if (!todo) return;
+
+    //console.log(todo);
+
+    return (
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col h-full space-y-2"
+      >
+        <input
+          defaultValue={todo.name}
+          {...register("name", { required: true })}
+          className="border-none rounded-md text-3xl p-2 focus:outline-none"
+        />
+        <textarea
+          defaultValue={todo.content}
+          {...register("content", { required: true })}
+          className="border-none rounded-md h-full resize-none p-2 focus:outline-none"
+        />
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="border-slate-600 px-4 py-2 text-xl rounded-lg bg-sky-300 hover:bg-sky-400 shadow-md"
+          >
+            Submit Entry
+          </button>
+        </div>
+      </form>
+    );
   }
-
-  const { todo } = query.data;
-
-  // TODO
-  if (!todo) return;
-
-  console.log(todo)
 
   return (
     <main className="bg-slate-300 h-screen">
@@ -51,29 +85,7 @@ export default function NotePage() {
       </div>
       <div className="w-screen h-[90%] bg-slate-300 text-slate-900 flex justify-center p-8 pt-4">
         <div className="w-full bg-slate-100 rounded-lg shadow-md p-2">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col h-full space-y-2"
-          >
-            <input
-              defaultValue={todo.name}
-              {...register("name", { required: true })}
-              className="border-none rounded-md text-3xl p-2 focus:outline-none"
-            />
-            <textarea
-              defaultValue={todo.content}
-              {...register("content", { required: true })}
-              className="border-none rounded-md h-full resize-none p-2 focus:outline-none"
-            />
-            <div className="flex justify-end">
-              <button
-                type="submit"
-                className="border-slate-600 px-4 py-2 text-xl rounded-lg bg-sky-300 hover:bg-sky-400 shadow-md"
-              >
-                Submit Entry
-              </button>
-            </div>
-          </form>
+          <NoteForm />
         </div>
       </div>
     </main>
