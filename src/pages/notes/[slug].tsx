8e2185eee67ffbc998ef92b30e9069a1ac13e59b
Ethan Mori <egmori2@gmail.com>
2023-11-25 11:17:57 -0500

    Back button, Add button, style

--- src/pages/notes/[slug].tsx
+++ src/pages/notes/[slug].tsx
@@ -1,13 +1,14 @@
-import { deleteTodo, getTodoById, getTodos, updateTodo } from "@/api/todos";
+import { addTodo, deleteTodo, getTodoById, getTodos, updateTodo } from "@/api/todos";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { BsFillTrashFill } from "react-icons/bs";
+import { BsArrowLeftShort, BsFillTrashFill, BsPlus, BsPlusCircle } from "react-icons/bs";
 
 export default function NotePage() {
   const listQuery = useQuery({ queryKey: ["getTodos"], queryFn: getTodos });
+  const queryClient = useQueryClient();
   const router = useRouter();
 
   const deleteMutation = useMutation(
@@ -17,10 +18,19 @@ export default function NotePage() {
     {
       onSuccess: () => {
         // queryClient.invalidateQueries({ queryKey: ["getTodos"] });
-        router.push("/notes") //TODO change to a better redirect behavior
+        router.push("/notes"); //TODO change to a better redirect behavior
       },
     }
   );
+
+  const addMutation = useMutation({
+    mutationFn: addTodo,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["getTodos"] });
+      queryClient.invalidateQueries({ queryKey: ["todoById"] });
+      router.push(`/notes/${JSON.parse(data).todo.id}`);
+    },
+  });
 
   if (!router.isReady) return;
 
@@ -28,8 +38,6 @@ export default function NotePage() {
 
   const slug = router.query.slug;
   console.log(slug);
-
-
 
   function NoteForm() {
     const queryClient = useQueryClient();
@@ -89,10 +97,10 @@ export default function NotePage() {
     if (!todo) return;
 
     //console.log(todo);
-    const str : string = watch("content")
+    const str: string = watch("content");
     const postLength = str ? str.length : 0;
     const MAX_POST_LENGTH = 1000;
-    
+
     return (
       <form
         onSubmit={handleSubmit(onSubmit)}
@@ -110,10 +118,12 @@ export default function NotePage() {
         />
         <div className="flex justify-end">
           <div className="px-4 py-2 text-xl">
-            <span className={postLength > MAX_POST_LENGTH ? "text-red-600" : ""}>
-            {postLength}/{MAX_POST_LENGTH}
+            <span
+              className={postLength > MAX_POST_LENGTH ? "text-red-600" : ""}
+            >
+              {postLength}/{MAX_POST_LENGTH}
             </span>
-            </div>
+          </div>
           <button
             type="submit"
             className="border-slate-600 px-4 py-2 text-xl rounded-lg bg-sky-300 hover:bg-sky-400 shadow-md"
@@ -138,19 +148,25 @@ export default function NotePage() {
       <div
         className={`border-b-2 ${
           isActive ? "bg-slate-200" : ""
-        } border-slate-200 last:border-0 px-2 py-1 rounded-md hover:bg-slate-200`}
+        } border-slate-200 last:border-0 rounded-md px-1 hover:bg-slate-200`}
       >
         <Link href={`/notes/${id}`}>
           <li key={id} className="flex justify-between">
-            <span className="text-lg">{name}</span>
-            {isActive ? 
-            <button onClick={() => {
-              //TODO confirm button
-              deleteMutation.mutate(id)
-            }}>
-              <BsFillTrashFill />
-            </button>
-             : <></>}
+            <span className="text-lg px-2 py-1">{name}</span>
+            {isActive ? (
+              <button
+                onClick={() => {
+                  //TODO confirm button
+                  deleteMutation.mutate(id);
+                }}
+              >
+                <div className="text-slate-600 hover:text-red-600 px-2 py-1 rounded-sm">
+                  <BsFillTrashFill />
+                </div>
+              </button>
+            ) : (
+              <></>
+            )}
           </li>
         </Link>
       </div>
@@ -165,7 +181,15 @@ export default function NotePage() {
 
   return (
     <main className="bg-slate-300 h-screen">
-      <div className="pt-4 px-8 w-screen flex justify-end">
+      <div className="pt-4 px-8 w-screen flex justify-between">
+        <Link href={"/notes"}>
+          <button className="border-slate-600 px-4 py-2 text-xl rounded-lg bg-sky-300 hover:bg-sky-400 shadow-md text-black flex">
+            <span className="relative top-1">
+              <BsArrowLeftShort />
+            </span>
+            Back
+          </button>
+        </Link>
         <button
           className="bg-slate-100 px-4 py-2 hover:bg-red-500 hover:text-white hover:shadow-md text-slate-900 text-xl rounded-md shadow-md"
           onClick={() => {
@@ -188,6 +212,19 @@ export default function NotePage() {
                 />
               );
             })}
+            <li key={0} className="flex justify-end">
+              <div className="py-2 px-1">
+                <button
+                  onClick={() => {
+                    addMutation.mutate();
+                  }}
+                >
+                  <div className="text-slate-600 px-1 rounded-sm text-2xl">
+                  <BsPlus />
+                  </div>
+                </button>
+              </div>
+            </li>
           </ul>
         </div>
         <div className="w-full bg-slate-100 rounded-lg shadow-md p-2">
