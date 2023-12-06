bbf48e06cf4c51df7f3dba4e29cab51fbacef4a3
Ethan Mori <egmori2@gmail.com>
2023-12-06 16:33:02 -0500

    Add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { SignInButton, SignOutButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
+import { BsArrowRightShort } from "react-icons/bs";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -38,15 +39,22 @@ export default function Home() {
     // mutation.mutate();
   };
 
-  console.log(watch("test"))
+  console.log(watch("test"));
   return (
-    <main className="flex flex-col items-center justify-center bg-slate-300 w-screen h-screen">
-      <div className="flex flex-col items-center justify-center bg-slate-100 text-slate-600 text-2xl p-4 rounded-md shadow-md font-semibold">
-        <span className="mb-4">Test page</span>
-        <SignInButton/>
-        <SignOutButton/>
-        <Link href="/notes">Notes Page</Link>
-      </div>
+    <main className="flex flex-col items-center justify-center bg-gradient-to-br from-slate-200 to-sky-200 w-screen h-screen text-slate-900 space-y-16">
+      <span className="font-semibold text-5xl">Journaling App</span>
+      <Link href={"/notes"}>
+        <button className="text-2xl font-semibold bg-sky-300 p-4 rounded-xl">
+          <div className="flex">
+
+            Get Started 
+            <span className="relative top-0 text-4xl">
+            <BsArrowRightShort/>
+
+            </span>
+          </div>
+        </button>
+      </Link>
     </main>
   );
 }
